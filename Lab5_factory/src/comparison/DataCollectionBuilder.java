3991748d9691b762cbbe7b1d0a27d6bafe77f598
Pontus <ndi14psd@student.hig.se>
2016-03-20 17:13:55 +0100

    Ny jämförelsemetod i DataCollectionBuilder, verkar gå att jämföra i
    vilken ordning man vill

--- Lab5_factory/src/comparison/DataCollectionBuilder.java
+++ Lab5_factory/src/comparison/DataCollectionBuilder.java
@@ -1,127 +1,71 @@
 package comparison;
 
-
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-
-/**
- * @author ntn13dcm
- * @author ofk14den
- * @version 2016-02-17
- *
- */
 public class DataCollectionBuilder {
-
-	private DataSource xData;
-	private DataSource yData;
+	private DataSource xSource;
+	private DataSource ySource;
 	private Resolution resolution;
+	private String title;
 
-	private Map<String, MatchedDataPair> finalResult;
-
-	/**
-	 * DataCollectionBuilder is the constructor of the DataCollectionBuilder
-	 * class.
-	 * 
-	 * @param xData
-	 *            DataSource
-	 * @param yData
-	 *            DataSource
-	 * @param resolution
-	 *            Resolution
-	 */
-	public DataCollectionBuilder(DataSource xData, DataSource yData, Resolution resolution) {
-		this.xData = xData;
-		this.yData = yData;
+	public DataCollectionBuilder(DataSource xSource, DataSource ySource, Resolution resolution) {
+		this.xSource = xSource;
+		this.ySource = ySource;
 		this.resolution = resolution;
-
 	}
 
-	/**
-	 * getTitle returns the names of the datasource 1 and 2 in a combined
-	 * sentence.
-	 * 
-	 * @return String
-	 */
+	public DataCollectionBuilder(DataSource xData, DataSource yData, Resolution resolution, String title) {
+		this(xData, yData, resolution);
+		this.title = title;
+	}
+
 	public String getTitle() {
-		return xData.getName() + "/" + yData.getName();
+		return (title != null) ? title : xSource.getName() + " / " + ySource.getName();
 	}
 
-	/**
-	 * creates a DataCollection and returns it.
-	 * 
-	 * @return DataCollection
-	 */
 	public DataCollection getResult() {
-		compileData();
-		DataCollection collection = new DataCollection(getTitle(), xData.getUnit(), yData.getUnit(), finalResult);
-
-		return collection;
+		Map<String, Double> xData = adjustToResolution(xSource.getData());
+		Map<String, Double> yData = adjustToResolution(ySource.getData());
+		Map<String, MatchedDataPair> finalResult = matchData(xData, yData);
+		
+		return new DataCollection(getTitle(), xSource.getUnit(), ySource.getUnit(), finalResult);
 	}
 
-	private Map<String, MatchedDataPair> compileData() {
-		MatchedDataPair matchDataPair;
-
-		finalResult = new HashMap<String, MatchedDataPair>();
-		String keyName = "";
-		String xPreviusKey = "";
-		int divValueX = 1, divValueY = 0;
-		Double xEqualValue = 0.0, yEqualValue = 0.0;
-
-		for (LocalDate xCurrentKey : xData.getData().keySet()) {
-
-			if (resolution.createLabel(xCurrentKey).equals(xPreviusKey) == false) {
-
-				xPreviusKey = resolution.createLabel(xCurrentKey);
-
-				if (yEqualValue != 0.0) {
-					xEqualValue = xEqualValue / divValueX;
-					yEqualValue = yEqualValue / divValueY;
-
-					matchDataPair = new MatchedDataPair(xEqualValue, yEqualValue);
-
-					finalResult.put(keyName, matchDataPair);
-
-					xEqualValue = 0.0;
-					yEqualValue = 0.0;
-					divValueX = 1;
-					divValueY = 0;
-				}
-				for (LocalDate comaprekey : yData.getData().keySet()) {
-
-					if (resolution.createLabel(xCurrentKey).equals(resolution.createLabel(comaprekey))) {
-						divValueY++;
-						yEqualValue = yEqualValue + yData.getData().get(comaprekey);
-					}
-				}
-				if (yEqualValue != 0.0)
-					xEqualValue = xData.getData().get(xCurrentKey);
-
-				keyName = resolution.createLabel(xCurrentKey);
-
-			} else {
-				xEqualValue = xEqualValue + xData.getData().get(xCurrentKey);
-				divValueX++;
-			}
-		}
-
-		if (yEqualValue != 0.0) {
-			xEqualValue = xEqualValue / divValueX;
-			yEqualValue = yEqualValue / divValueY;
-
-			matchDataPair = new MatchedDataPair(xEqualValue, yEqualValue);
-
-			finalResult.put(keyName, matchDataPair);
-
-			xEqualValue = 0.0;
-			yEqualValue = 0.0;
-			divValueX = 1;
-			divValueY = 0;
-		}
-
-		return finalResult;
+	private Map<String, Double> adjustToResolution(Map<LocalDate, Double> data) {
+		Map<String, List<Double>> result = getWithProperResolution(data);
+		return result.keySet().stream()
+				.collect(Collectors.toMap(date -> date, date -> roundTwoDecimals(averageOfList(result.get(date)))));
 	}
+
+	private Map<String, List<Double>> getWithProperResolution(Map<LocalDate, Double> data) {
+		Map<String, List<Double>> result = new TreeMap<>();
+		for (Entry<LocalDate, Double> entry : data.entrySet()) {
+			result.merge(resolution.createLabel(entry.getKey()), Arrays.asList(entry.getValue()), 
+					(existingList, newList) -> Stream.concat(existingList.stream(), newList.stream()).collect(Collectors.toList()));
+		}
+		return result;
+	}
+
+	private double averageOfList(List<Double> list) {
+		return list.stream().mapToDouble(Double::valueOf).average().getAsDouble();
+	}
+
+	private double roundTwoDecimals(double d) {
+		return ((double) Math.round(d * 100) / 100);
+	}
+
+	private Map<String, MatchedDataPair> matchData(Map<String, Double> xData, Map<String, Double> yData) {
+		return xData.keySet().stream().filter(yData::containsKey)
+				.collect(Collectors.toMap(key -> key, key -> new MatchedDataPair(xData.get(key), yData.get(key))));
+	}
+
+
 }
